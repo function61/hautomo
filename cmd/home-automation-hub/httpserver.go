5316c5acc78e31601881bddf77ea357477e12172
Joonas Loppi <joonas@joonas.fi>
2019-01-19 21:25:59 +0200

    very rudimentary UI

--- cmd/home-automation-hub/httpserver.go
+++ cmd/home-automation-hub/httpserver.go
@@ -6,11 +6,56 @@ import (
 	"github.com/function61/gokit/logex"
 	"github.com/function61/gokit/stopper"
 	"github.com/function61/home-automation-hub/pkg/hapitypes"
+	"html/template"
 	"log"
 	"net/http"
+	"sort"
+	"time"
 )
 
-func handleHttp(conf *hapitypes.ConfigFile, logger *log.Logger, stop *stopper.Stopper) {
+const tpl = `
+<html>
+<head>
+	<title>home-automation-hub</title>
+</head>
+<body>
+
+<table>
+<thead>
+<tr>
+	<th></th>
+	<th>name</th>
+	<th>type</th>
+	<th>battery</th>
+	<th>link quality</th>
+	<th>last online</th>
+	<th>temp</th>
+</tr>
+</thead>
+<tbody>
+{{range .}}
+<tr>
+	<td>{{.Device.ProbablyTurnedOn}}</td>
+	<td>{{.Device.Conf.DeviceId}}</td>
+	<td>{{.Device.DeviceType.Manufacturer}} {{.Device.DeviceType.Model}}</td>
+	<td title="type: {{.Device.DeviceType.BatteryType}} voltage: {{.Device.BatteryVoltage}} mV">{{.Device.BatteryPct}} %</td>
+	<td>{{.Device.LinkQuality}} %</td>
+	<td>{{.LastOnlineFormatted}}</td>
+	<td>{{if .Device.LastTemperatureHumidityPressureEvent}}
+		temp {{.Device.LastTemperatureHumidityPressureEvent.Temperature}}
+		humidify {{.Device.LastTemperatureHumidityPressureEvent.Humidity}}
+		pressure {{.Device.LastTemperatureHumidityPressureEvent.Pressure}}
+	{{end}}</td>
+</tr>
+{{end}}
+</tbody>
+</table>
+
+</body>
+</html>
+`
+
+func handleHttp(app *Application, conf *hapitypes.ConfigFile, logger *log.Logger, stop *stopper.Stopper) {
 	logl := logex.Levels(logger)
 
 	defer stop.Done()
@@ -28,6 +73,50 @@ func handleHttp(conf *hapitypes.ConfigFile, logger *log.Logger, stop *stopper.St
 		enc := json.NewEncoder(w)
 		enc.SetIndent("", "  ")
 		enc.Encode(conf)
+	})
+
+	http.HandleFunc("/ui", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.New("name").Parse(tpl)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		devices := []*hapitypes.Device{}
+
+		for _, dev := range app.deviceById {
+			devices = append(devices, dev)
+		}
+
+		sort.Slice(devices, func(i, j int) bool {
+			return devices[i].Conf.DeviceId < devices[j].Conf.DeviceId
+		})
+
+		type DeviceWithComputed struct {
+			Device              *hapitypes.Device
+			LastOnlineFormatted string
+		}
+
+		now := time.Now()
+
+		devicesComputed := []DeviceWithComputed{}
+		for _, device := range devices {
+			lastOnlineFormatted := ""
+
+			if device.LastOnline != nil {
+				lastOnlineFormatted = now.Sub(*device.LastOnline).String()
+			}
+
+			devicesComputed = append(devicesComputed, DeviceWithComputed{
+				Device:              device,
+				LastOnlineFormatted: lastOnlineFormatted,
+			})
+		}
+
+		if err := tmpl.Execute(w, devicesComputed); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	})
 
 	logl.Info.Printf("Starting to listen at %s", srv.Addr)
