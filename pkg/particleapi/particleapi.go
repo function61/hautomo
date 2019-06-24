b995e522110381e4895dfc8de28c98d5e121cbf4
Joonas Loppi <joonas@joonas.fi>
2019-06-24 17:53:21 +0300

    particleapi: use ezhttp

--- pkg/particleapi/particleapi.go
+++ pkg/particleapi/particleapi.go
@@ -1,31 +1,24 @@
 package particleapi
 
 import (
-	"fmt"
-	"net/http"
+	"context"
+	"github.com/function61/gokit/ezhttp"
 	"net/url"
 	"strings"
 )
 
 func Invoke(device string, command string, arg string, accessToken string) error {
-	apiEndpoint := "https://api.particle.io/v1/devices/" + device + "/" + command
-
 	urlValues := url.Values{}
 	urlValues.Add("access_token", accessToken)
 	urlValues.Add("arg", arg)
 
-	req, _ := http.NewRequest("POST", apiEndpoint, strings.NewReader(urlValues.Encode()))
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	ctx, cancel := context.WithTimeout(context.TODO(), ezhttp.DefaultTimeout10s)
+	defer cancel()
 
-	httpClient := http.Client{}
-	response, errTransport := httpClient.Do(req)
-	if errTransport != nil {
-		return errTransport
-	}
+	_, err := ezhttp.Post(
+		ctx,
+		"https://api.particle.io/v1/devices/"+device+"/"+command,
+		ezhttp.SendBody(strings.NewReader(urlValues.Encode()), "application/x-www-form-urlencoded"))
 
-	if response.StatusCode < 200 || response.StatusCode > 299 {
-		return fmt.Errorf("Response not 2xx: got %d", response.StatusCode)
-	}
-
-	return nil
+	return err
 }
