885d1d6f99e0d9cb916799f0663a3ea4d8f33e10
Joonas Loppi <joonas@joonas.fi>
2020-12-22 17:28:48 +0200

    remove JS-based lambda-alexa-connector

--- build-alexaconnector.Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM fn61/buildkit-js:20181011_0937_e1806603
-
-CMD bin/build.sh
--- lambda-alexa-connector/bin/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-
-source /build-common.sh
-
-BINARY_NAME="home-automation-hub-alexaconnector"
-BINTRAY_PROJECT="function61/home-automation-hub"
-
-standardBuildProcess
